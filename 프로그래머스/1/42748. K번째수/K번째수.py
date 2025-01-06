b88f93eee07c79fee5bb94ed80d20e0d590f288b
Junanida <115341498+junanida@users.noreply.github.com>
2025-01-06 22:47:15 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -6,3 +6,6 @@ def solution(array, commands):
         temp.sort()
         answer.append(temp[commands[i][2]-1])
     return answer
+
+# def solution(array, commands):
+#     return list(map(lambda x:sorted(array[x[0]-1:x[1]])[x[2]-1], commands))
