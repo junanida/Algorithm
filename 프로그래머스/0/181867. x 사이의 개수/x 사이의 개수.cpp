ada4846bbe81ff01e508c9a9f8acd6acc3a37270
Junanida <wnsghks168@naver.com>
2025-04-03 23:13:43 +0900

    [level 0] Title: x 사이의 개수, Time: 4.91 ms, Memory: 7.22 MB -BaekjoonHub

---
+++ 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(string myString) {
+    vector<int> answer;
+    int cnt = 0;
+    for(int i = 0; i < myString.size(); i++)
+    {
+        if(myString[i] == 'x')
+        {
+            answer.push_back(cnt);
+            cnt = 0;
+        }
+        else
+        {
+            cnt++;
+        }
+        if(i == myString.size()-1)
+        {
+            answer.push_back(cnt);
+        }
+    }
+    return answer;
+}
