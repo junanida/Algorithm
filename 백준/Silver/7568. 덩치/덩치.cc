06239ecc0f4c40215f78259e53959c6a5b5aa557
Junanida <wnsghks168@naver.com>
2025-09-04 17:53:14 +0900

    [Silver V] Title: 덩치, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int N;
+	cin >> N;
+
+	vector<pair<int, int>> people(N);
+
+	for (int i = 0; i < N; i++)
+	{
+		int weight, height;
+		cin >> weight >> height;
+		people[i] = { weight, height };
+	}
+
+	for (int i = 0; i < N; i++)
+	{
+		int rank = 1;
+		for (int j = 0; j < N; j++)
+		{
+			if (i == j) continue;
+			if (people[j].first > people[i].first && people[j].second > people[i].second)
+			{
+				rank++;
+			}
+		}
+		cout << rank << " ";
+	}
+	return 0;
+}
